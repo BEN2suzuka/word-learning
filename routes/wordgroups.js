90c9dd5cf18260dda152eceeee7b39292463c976
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-06-24 09:14:29 +0000

    「所属単語一覧表示」「未分類グループを作成できるをテスト」を実装

--- routes/wordgroups.js
+++ routes/wordgroups.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const User = require('../models/user');
+const Word = require('../models/word');
 const Wordgroup = require('../models/wordgroup');
 const uuidv4 = require('uuid/v4');
 
@@ -17,13 +19,28 @@ router.post('/wordgroups/firstgroup', authenticationEnsurer, (req, res, next) =>
 });
 
 router.get('/wordgroups/:wordGroupId', authenticationEnsurer, (req, res, next) => {
-  Wordgroup.findAll({
-    where: { createdBy: req.user.id }
-  }).then((wordGroups) => {
+  let storedWordGroup = null;
+  Wordgroup.findOne({
+    include: [{
+      model: User,
+      attributes: ['userId', 'username']
+    }],
+    where: { wordGroupId: req.params.wordGroupId }
+  }).then((wordGroup) => {
+    if (wordGroup) {
+      storedWordGroup = wordGroup;
+      return Word.findAll({
+        where: { wordGroupId: wordGroup.wordGroupId }
+      });
+    } else {
+      const err = new Error('指定された単語グループは見つかりません');
+      err.status = 404;
+      next(err);
+    }
+  }).then((words) => {
     res.render('wordgroup', {
-      title: 'やあ',
-      user: req.user,
-      wordGroups: wordGroups
+      wordGroup: storedWordGroup,
+      words: words
     });
   });
 });
--- test/test.js
+++ test/test.js
@@ -2,9 +2,10 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+const User = require('../models/user');
+const Wordgroup = require('../models/wordgroup');
 
 describe('/', () => {
-
   it('ログインのためのリンクが含まれる', (done) => {
     request(app)
       .get('/')
@@ -12,14 +13,12 @@ describe('/', () => {
       .expect(/<a href="\/login"/)
       .expect(200, done);
   });
-
 });
 
 describe('/', () => {
-
   before(() => {
     passportStub.install(app);
-    passportStub.login({ displayName: 'testuser' });
+    passportStub.login({ username: 'testuser' });
   });
 
   after(() => {
@@ -28,10 +27,71 @@ describe('/', () => {
   });
 
   it('ログイン時はユーザー名が表示される', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .get('/')
+        .expect(/testuser/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          User.findOne({ where: { userId: 0 } }).then((u) => {
+            u.destroy().then(() => {
+              done();
+            });
+          });
+        });
+    });
+  });
+});
+
+describe('/logout', () => {
+  it('/ にリダイレクトされる', (done) => {
     request(app)
-      .get('/')
-      .expect(/testuser/)
-      .expect(200, done);
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done);
+  });
+});
+
+describe('/words/wordgroups', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('「未分類」を作成できる', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/words/wordgroups/firstgroup')
+        .expect('Location', '/words/wordgroups/:wordGroupId')
+        .expect(302)
+        .end((err, res) => {
+          const createdWordGroupPath = res.headers.location;
+          request(app)
+            .get(createdWordGroupPath)
+            .expect(/未分類/)
+            .expect(/testuser/)
+            .expect(200)
+            .end((err, res) => {
+              if (err) return done(err);
+              const wordGroupId = createdWordGroupPath.split('/wordgroups/')[1];
+              Wordgroup.findOne({ where: { wordGroupId: wordGroupId } }).then((w) => {
+                w.destroy().then(() => {
+                  User.findOne({ where: { userId: 0 } }).then((u) => {
+                    u.destroy().then(() => {
+                      done();
+                    });
+                  });
+                });
+              });
+            });
+        });
+    });
   });
 
 });
