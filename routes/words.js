49117749125ff342bad69680dcda4fcfb54f31bb
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-06-27 12:42:51 +0000

    単語の削除機能を実装

--- routes/words.js
+++ routes/words.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
 const Word = require('../models/word');
+const Wordgroup = require('../models/wordgroup');
 const Memory = require('../models/memory');
 
 router.post('/new', authenticationEnsurer, (req, res, next) => {
@@ -23,5 +24,43 @@ router.post('/new', authenticationEnsurer, (req, res, next) => {
     res.redirect('/wordgroups/' + req.body.wordGroupId);
   });
 });
+
+router.post('/:wordId/users/:userId/delete', authenticationEnsurer, (req, res, next) => {
+  let storedWord = null;
+  Word.findOne({
+    where: { wordId: req.params.wordId }
+  }).then((word) => {
+    storedWord = word;
+    if (isMine(req, word)) {
+      Memory.findOne({
+        where: { userId: req.user.id, wordId: storedWord.wordId }
+      }).then((memory) => {
+        memory.destroy().then(() => {
+          storedWord.destroy().then(() => {
+            Wordgroup.findAll({
+              where: { createdBy: req.user.id },
+            }).then((wordGroups) => {
+              res.render('index', {
+                title: '単語帳アプリ',
+                user: req.user,
+                wordGroups: wordGroups
+              });
+            });
+          });
+        });
+      });
+    } else {
+      const err = new Error('指定された単語がない、または、削除する権限がありません');
+      err.status = 404;
+      next(err);
+    }
+  })
+})
+
+function isMine(req, word) {
+  return word && word.createdBy === req.user.id;
+}
+
+
 
 module.exports = router;
