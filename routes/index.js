93b06a44bd91d65b56ddb79bf90b5cbe1984fade
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-07-29 06:35:28 +0000

    トップページのデザイン完成

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,21 @@ const Wordgroup = require('../models/wordgroup');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const title = '単語帳アプリ'
   if (req.user) {
     Wordgroup.findAll({
       where: {createdBy: req.user.id},
     }).then((wordGroups) => {
       if (wordGroups) {
       res.render('index', {
-        title: title,
         user: req.user,
         wordGroups: wordGroups
       });
     } else {
-      res.render('index', { title: title, user: req.user });
+      res.render('index', { user: req.user });
     }
     });
   } else {
-    res.render('index', { title: title, user: req.user });
+    res.render('index', { user: req.user });
   }
 });
 
