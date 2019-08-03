f5795dfbaef12d1d40f94ccf6bb636d7d7bffb76
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-08-03 09:49:42 +0000

    テストコードdelay関数を実装

--- test/test.js
+++ test/test.js
@@ -1,24 +1,32 @@
 'use strict';
 const request = require('supertest');
+const assert = require('assert');
 const app = require('../app');
 const passportStub = require('passport-stub');
 const User = require('../models/user');
 const Wordgroup = require('../models/wordgroup');
+const Word = require('../models/word');
+const Memory = require('../models/memory');
+const Favorite = require('../models/favorite');
 
 describe('/', () => {
+
   it('ログインのためのリンクが含まれる', (done) => {
     request(app)
       .get('/')
       .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(/<a href="\/login"/)
+      .expect(/<a.*href="\/login"/)
       .expect(200, done);
   });
+
 });
+
 
 describe('/', () => {
+
   before(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ userId: 123456789, username: 'testuser' });
   });
 
   after(() => {
@@ -26,72 +34,31 @@ describe('/', () => {
     passportStub.uninstall(app);
   });
 
-  it('ログイン時はユーザー名が表示される', (done) => {
-    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
-      request(app)
+  it('ログイン時はユーザー名が表示される', () => {
+    console.log('いまここ１');
+    return User.upsert({ userId: 123456789, username: 'testuser' })
+    .then(() => {
+      console.log('いまここ２');
+      return delay(2000);    // ここで2秒足止めして、データベースのスタンバイ待ち
+    })
+    .then(() => {
+      console.log('いまここ３');
+      return request(app)
         .get('/')
-        .expect(/testuser/)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          User.findOne({ where: { userId: 0 } }).then((u) => {
-            u.destroy().then(() => {
-              done();
-            });
-          });
-        });
-    });
-  });
-});
-
-describe('/logout', () => {
-  it('/ にリダイレクトされる', (done) => {
-    request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302, done);
-  });
-});
-
-describe('/wordgroups', () => {
-  before(() => {
-    passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
-  });
-
-  after(() => {
-    passportStub.logout();
-    passportStub.uninstall(app);
-  });
-
-  it('「未分類」を作成できる', (done) => {
-    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
-      request(app)
-        .post('/wordgroups/firstgroup')
-        .expect('Location', '/wordgroups/:wordGroupId')
-        .expect(302)
-        .end((err, res) => {
-          const createdWordGroupPath = res.headers.location;
-          request(app)
-            .get(createdWordGroupPath)
-            .expect(/未分類/)
-            .expect(/testuser/)
-            .expect(200)
-            .end((err, res) => {
-              if (err) return done(err);
-              const wordGroupId = createdWordGroupPath.split('/wordgroups/')[1];
-              Wordgroup.findOne({ where: { wordGroupId: wordGroupId } }).then((w) => {
-                w.destroy().then(() => {
-                  User.findOne({ where: { userId: 0 } }).then((u) => {
-                    u.destroy().then(() => {
-                      done();
-                    });
-                  });
-                });
-              });
-            });
-        });
+        .expect(/testuser をログアウト/)
+        .expect(200);
     });
   });
 
 });
+
+
+function delay(timeoutMs) {
+  console.log('delay開始');
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('delay完了');
+      resolve(timeoutMs);
+    }, timeoutMs);
+  });
+}
